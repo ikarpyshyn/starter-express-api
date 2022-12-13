ec5ebb48e8991c7a9c761ad85d0ed8a60acbbff5
ikarpyshyn <ihor.karpyshyn@gmail.com>
2022-12-13 00:38:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Hi its Ihor~~~~!')
+    res.send('Hi its Ihor~~~1~!')
 })
 app.listen(process.env.PORT || 3000)
